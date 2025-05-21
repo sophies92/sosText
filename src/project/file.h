e5d47093f4f58cc6d05ca2c62eb5b7ed3ecb0c29
sophie short <sos66@aber.ac.uk>
2025-05-21 15:56:10 +0100

    updates to file.h

--- src/project/file.h
+++ src/project/file.h
@@ -12,6 +12,22 @@ namespace Project
         File();
         ~File();
 
+        /**
+         * @fn checkExists()
+         * @brief check if the file exists
+         */
+        bool checkExists();
+
+        /**
+         * @fn checkFileType()
+         * @brief checks if the file is an openable file type
+         */
+        int checkFileType();
+
+        /**
+         * @fn loadFile()
+         * @brief loads the file and returns it as a string
+         */
         std::string *loadFile();
     };
 }
