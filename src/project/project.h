a33a1ec3af282d8f3af818da7276dd3e21e3b6b4
sophie short <sos66@aber.ac.uk>
2025-05-22 13:09:24 +0100

    added project class

---
+++ src/project/project.h
@@ -0,0 +1,35 @@
+/**
+ * @file project.h
+ * @brief 
+ * @author Sophie Short
+ */
+
+#ifndef PROJECT_H
+#define PROJECT_H
+
+namespace Project
+{
+    /**
+     * @class Project::Project
+     * @brief this class holds all the information about the currently open Project.
+     */
+    class Project
+    {
+        // Public Variables
+        public:
+
+        // Private Variables
+        private:
+
+        // Public Functions
+        public:
+        Project();
+        ~Project();
+
+        // Private Functions
+        private:
+        
+    };
+}
+
+#endif // PROJECT_H
