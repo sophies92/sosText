c67b890ba0a1f77460baedf350040135536a4674
Sophie <sophiie1592@gmail.com>
2025-10-13 19:57:34 +0100

    todo

--- src/findToolbar/findToolbar.cpp
+++ src/findToolbar/findToolbar.cpp
@@ -155,6 +155,7 @@ void Sosware::SosText::FindToolbar::resetFindToolbar()
 
 void Sosware::SosText::FindToolbar::nextButtonPressed()
 {
+    // TODO change focus to text window when search/next/prev buttons are pressed
     if(stringMatchesFound > 0)
     {
         if(currentMatchIndex < (stringMatchesFound - 1))
